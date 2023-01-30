6e684618069b1b9ace20918c6f3aa11be7ec2fa1
Nataniel Porto <nataniel.porto@bubble-go.pt>
2023-01-30 00:17:29 +0000

    molecule/tag: add tag molecule

---
+++ src/components/Molecules/Tag/Tag.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Octicons } from "@expo/vector-icons";
+import { colors } from "../../../global/colors";
+export const Tag = () => {
+  return (
+    <View style={styles.container}>
+      <Octicons
+        name="database"
+        size={12}
+        color={colors.purple}
+        style={{ marginRight: 5 }}
+      />
+      <Text style={styles.text}>Earn Rewards</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.lightPurple,
+    paddingHorizontal: 8,
+    paddingVertical: 9,
+    maxWidth: 110,
+    borderRadius: 4,
+  },
+  text: { color: colors.purple, fontSize: 11 },
+});
--- src/global/colors.ts
+++ src/global/colors.ts
@@ -2,9 +2,11 @@ export const colors = {
   primary: "#770fdf",
   secondary: "#0fdf8f",
   danger: "#EE8688",
+
   white: "#fff",
   black: "#000",
   purple: "#770FDF",
+  lightPurple: "#F7EFFF",
   yellow: "#F0A719",
   blue: "#4A88D0",
 
@@ -12,9 +14,6 @@ export const colors = {
   grey300: "#e6e6e6",
   grey500: "#CFCFCF",
   grey700: "#a0a0a0",
-
-  textLight: "#fff",
-  textDark: "#000",
 
   greyBackground: "#f4f4f4",
   greyBackground100: "#F5F5F5",
